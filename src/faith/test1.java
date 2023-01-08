ade8cc1be8bd093b938d90eb728c05ced66408f9
FaithSlade <faithwslade@gmail.com>
2023-01-07 21:18:17 -0800

    added changed2 from your requested

--- src/faith/test1.java
+++ src/faith/test1.java
@@ -4,5 +4,9 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("Hello B28");
         System.out.println("new change");
+
+        for (int i = 0; i < 10 ; i++) {
+            System.out.println("please accept my pull-request");
+        }
     }
 }
